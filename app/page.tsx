aabeb4bde8dcfaa17495b544f3a386f306e03567
Agnibha Ray <agnibharay.55@gmail.com>
2024-03-24 20:04:23 +0530

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import { Heading } from "@/components/heading";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Card>
+        <CardHeader>
+          <CardTitle>SELECT AN ANSWER FROM THE DROP DOWN</CardTitle>
+        </CardHeader>
+        <CardContent>
+          
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
